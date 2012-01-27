a60fba3b3be75bca4d8069def6e529a08d9c2e78
Larry Smithmier <HRNUGReportingDemo@smithmier.com>
2012-01-27 03:15:56 -0500

    Remove Constants

--- GeeklistSharp.Tests/TestConstants.cs
+++ GeeklistSharp.Tests/TestConstants.cs
@@ -7,10 +7,10 @@ namespace GeeklistSharp.Tests
 {
     public class TestConstants
     {
-//#error Add Your Geekli.st Keys here
-        public const string OAUTH_CONSUMER_KEY = "sdrLsA7FQykDlfNMPQ1CKyIhbu4";
-        public const string OAUTH_CONSUMER_SECRET = "m4OWfKmWeYvOwvjS7zJRUSAJlJRICSYLQfmQaJtHrOU";
-        public const string TOKEN = "XJ4ANegC39ca90_ydSl9iVmF2R8";
-        public const string TOKEN_SECRET = "nJpKopGKzUQ4klaiIAWnPDQ9PypX7vZK9t_OL-zuE-E";
+#error Add Your Geekli.st Keys here
+        public const string OAUTH_CONSUMER_KEY = "";
+        public const string OAUTH_CONSUMER_SECRET = "";
+        public const string TOKEN = "";
+        public const string TOKEN_SECRET = "";
     }
 }
