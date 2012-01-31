a8a060fae6899eeed9e3c07c0ae0570ac134658e
Mateus Carvalho <oakcool@yahoo.com>
2012-01-30 19:18:55 -0800

    more refactoring

--- GeeklistSharp/Model/GeekListException.cs
+++ GeeklistSharp/Model/GeekListException.cs
@@ -9,9 +9,9 @@ namespace GeeklistSharp.Model
 	{
 		public string Status { get; private set; }
 
-        public Error Error { get; set; }
+        public GeeklistError Error { get; set; }
 
-		public GeekListException(string status, Error error)
+		public GeekListException(string status, GeeklistError error)
 			: base(string.Format("API call resulted in status: {0}", status))
 		{
 			this.Status = status;
