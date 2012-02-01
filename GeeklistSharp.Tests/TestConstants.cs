7371be35729aaf8ff6e1458c0c2cfe393c323066
Mateus Carvalho <oakcool@yahoo.com>
2012-01-31 21:24:21 -0800

    removing test key lol

--- GeeklistSharp.Tests/TestConstants.cs
+++ GeeklistSharp.Tests/TestConstants.cs
@@ -11,10 +11,10 @@ namespace GeeklistSharp.Tests
     {
 
 //#error Add Your Geekli.st Keys here
-        public const string OAUTH_CONSUMER_KEY = "WRP0IToyEfSym6WjALL_ryfA18s";
-        public const string OAUTH_CONSUMER_SECRET = "HDOiP47DT4S7axLc2ai2Zv4UVGNXXerh6yNcdRNSBno";
-        public const string TOKEN = "UVytN56pN9260cqG6AmAGm_v-wE";
-        public const string TOKEN_SECRET = "ZT0IQJxySPfr-HNgbbDinsFUCwHDANsgxyK7LQeVavU";
+        public const string OAUTH_CONSUMER_KEY = "";
+        public const string OAUTH_CONSUMER_SECRET = "";
+        public const string TOKEN = "";
+        public const string TOKEN_SECRET = "";
         public const string CARDID = "146a1bcbe95def14a19a5441cbccb17f5b7b06b25b99396ac906872e584b268a";
         
         public static readonly Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
